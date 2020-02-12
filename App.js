35de0cbd7dd7dd0ff8ec94a26ce12ccb13645bf0
Kamil Rusin <lotrusek@gmail.com>
2020-02-12 15:37:44 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Init</Text>
+    </>
+  );
+};
+
+export default App;
