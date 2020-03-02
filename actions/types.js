8b8dca3c99a4a6bf4b6796dc9c80ebf2c69acd69
Kamil Rusin <lotrusek@gmail.com>
2020-03-02 13:46:00 +0100

    Initial commit for redux

---
+++ actions/types.js
@@ -0,0 +1,3 @@
+export const REFRESH_LIST = 'REFRESH_LIST';
+export const SORT_BY_AUTHOR = 'SORT_BY_AUTHOR';
+export const SORT_BY_ID = 'SORT_BY_ID';
---
+++ reducers/listReducer.js
@@ -0,0 +1,5 @@
+import {REFRESH_LIST, SORT_BY_AUTHOR, SORT_BY_ID} from 'actions/types';
+
+const initialState = {
+  data: [],
+};
