774e86ab0a9e2322f5811771c6f6627dcbb9afb7
Kamil Rusin <lotrusek@gmail.com>
2020-03-04 13:38:43 +0100

    feat: initialize react-navigation

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import PicturesListContainer from './components/PicturesListContainer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const HomeScreen = () => {
+  return (
+    <PicturesListContainer/>
+  );
+};
+
+const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
-  return <PicturesListContainer />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>);
 };
 
 export default App;
