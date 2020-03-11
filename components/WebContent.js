9ccdbcb2964f054a58202ff54bb65ac13024c447
Kamil Rusin <lotrusek@gmail.com>
2020-03-11 11:52:37 +0100

    refactor: change WebContent into functional component

--- components/WebContent.js
+++ components/WebContent.js
@@ -1,20 +1,14 @@
 import React, {Component} from 'react';
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
 
-class WebContent extends Component {
-  constructor(props) {
-    super(props);
-  }
+const WebContent = props => {
+  const {pageUrl} = props;
 
-  render() {
-    const {pageUrl} = this.props;
-
-    return (
-      <WebView
-        source={{ uri: pageUrl }}
-      />
-    );
-  }
-}
+  return (
+    <WebView
+      source={{uri: pageUrl}}
+    />
+  );
+};
 
 export default WebContent;
