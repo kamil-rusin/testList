920c1e3688378563c514f33c554dd525451bf27c
Kamil Rusin <lotrusek@gmail.com>
2020-03-17 12:44:51 +0100

    feat: add redux-persist support

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistReducer } from 'redux-persist';
 
 import listReducer from './reducers/listReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['listReducer'],
+};
 
 const rootReducer = combineReducers({
     listReducer: listReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleware = [thunk];
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(...middleware));
+const configureStore = () => createStore(persistedReducer, applyMiddleware(...middleware));
 
 export default configureStore;
