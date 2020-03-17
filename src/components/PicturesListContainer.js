a235509e879da0ac2daf54a0089dfb2f71dd0aca
Kamil Rusin <lotrusek@gmail.com>
2020-03-17 13:27:10 +0100

    fix: hide splashScreen after app loading

--- src/components/PicturesListContainer.js
+++ src/components/PicturesListContainer.js
@@ -5,6 +5,7 @@ import PicturesListComponent from './PicturesListComponent';
 import fetchDataAction from '../redux/actions/fetchData';
 import filterDataAction from '../redux/actions/filterData';
 import TextFilterElement from './TextFilterElement';
+import SplashScreen from 'react-native-splash-screen';
 
 const getSearchKey = (state) => state.listReducer.searchKey;
 const getDataToFilter = (state) => state.listReducer.data;
@@ -33,6 +34,7 @@ const PictureListContainer = (props) => {
     }, [dispatch]);
 
     useEffect(() => {
+        SplashScreen.hide();
         fetchData();
     }, [fetchData]);
 
