53a4e0c6e4c93d77e5ba5ca8b3368f5e96b6616d
Kamil Rusin <lotrusek@gmail.com>
2020-03-19 14:20:50 +0100

    feat: add alert for invalid url, add control over reactivating the scanner

--- src/screens/QRScannerScreen.js
+++ src/screens/QRScannerScreen.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 const QRScannerScreen = ({ navigation }) => {
+    const [reactivate, setReactivate] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+            setReactivate(true);
+            setIsFocused(true);
+        });
+
+        return unsubscribe;
+    }, [navigation]);
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('blur', () => {
+            setReactivate(false);
+            setIsFocused(false);
+        });
+
+        return unsubscribe;
+    }, [navigation]);
+
     const onSuccess = (data) => {
         const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
         let found;
 
         if ((found = regex.exec(data))) {
-            console.log(found);
             loadInBrowser(found[0]);
+        } else {
+            showInvalidCodeAlert();
         }
     };
 
@@ -17,14 +39,32 @@ const QRScannerScreen = ({ navigation }) => {
         navigation.push('WebContent', { url: url });
     };
 
+    const showInvalidCodeAlert = () => {
+        Alert.alert(
+            'Error',
+            'Invalid url code.',
+            [
+                {
+                    text: 'OK',
+                    onPress: () => setReactivate(true),
+                },
+            ],
+            { cancelable: false },
+        );
+    };
+
     return (
         <View style={styles.view}>
-            <QRCodeScanner
-                fadeIn={true}
-                containerStyle={styles.container}
-                vibrate={true}
-                onRead={(e) => onSuccess(e.data)}
-            />
+            {isFocused && (
+                <QRCodeScanner
+                    fadeIn={true}
+                    containerStyle={styles.container}
+                    reactivateTimeout={2000}
+                    reactivate={reactivate}
+                    vibrate={true}
+                    onRead={navigation.isFocused() ? (e) => onSuccess(e.data) : null}
+                />
+            )}
         </View>
     );
 };
