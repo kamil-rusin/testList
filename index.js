b135fd0263fcf6148a6e6650093e539ba788d2ee
Kamil Rusin <48494453+kamil-rusin@users.noreply.github.com>
2020-03-19 11:20:01 +0100

    Add persist (#15)
    * refactor: change the code to match lint/prettier rules
    
    * refactor: change project structure and change hook for fetchData to match lint
    
    * fix: resolve merge conflict
    
    * refactor: use callback in functions
    
    * feat: add redux-persist support
    
    Co-authored-by: Łukasz Kubica <kubicalukasz92@gmail.com>

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ import { AppRegistry } from 'react-native';
 import App from './src/screens/App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 import configureStore from './src/redux/store';
 import React from 'react';
 
 const store = configureStore();
 
+const persistedStore = persistStore(store);
+
 const TestList = () => (
     <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistedStore} loading={null}>
+            <App />
+        </PersistGate>
     </Provider>
 );
 
